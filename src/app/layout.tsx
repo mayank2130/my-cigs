ab6fc2535bf30b8278d1611e4fdb1cf85d2877b9
mayank2130 <mayankthakur1712@gmail.com>
2024-12-23 15:36:37 +0530

    adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4633819125659882"
+          // @ts-expect-error: Google AdSense script requires crossorigin attribute
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
